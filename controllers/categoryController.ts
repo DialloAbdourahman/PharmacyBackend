fa8926ebf6043c8b8251d3e18ec7a39b083b0932
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2023-07-15 17:14:25 +0100

    images and category images

--- controllers/categoryController.ts
+++ controllers/categoryController.ts
@@ -8,9 +8,22 @@ const prisma: PrismaClient<
 
 const seeCategories = async (req: Request, res: Response) => {
   try {
+    // Get all the categories from db.
     const categories = await prisma.productCategory.findMany({});
 
-    res.status(200).json(categories);
+    // Map the correct url
+    let categoriesWithImagesUrl = categories.map((category) => {
+      if (category.imageUrl !== '') {
+        return {
+          ...category,
+          imageUrl: `http://localhost:4000/static/categoryImages/${category.imageUrl}`,
+        };
+      }
+      return category;
+    });
+
+    // Send a positive response with all the categories.
+    res.status(200).json(categoriesWithImagesUrl);
   } catch (error) {
     return res.status(500).json({ message: 'Something went wrong.', error });
   }
