66f0b2286c0d468210806d3520305eee2206ecda
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2023-07-19 18:13:02 +0100

    error fix

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -7,8 +7,6 @@ const prisma: PrismaClient<
 > = require('../utils/prismaClient');
 
 const getProducts = async (req: Request, res: Response) => {
-  console.log(req.body);
-
   try {
     // Get required data from request body and modify it a bit.
     let {
